3a9b516b3f95f45e117ff7b11556b82b22a63ea2
MahdiRezaie <103851352+Mahd25@users.noreply.github.com>
2024-06-10 13:14:15 -0700

    change ajax of footer

--- seasons-css-&-js/Django-seasons.js
+++ seasons-css-&-js/Django-seasons.js
@@ -47,31 +47,33 @@ $(document).ready(function() {
         }
     });
 
-    // add footer to end body
+    // Load footer and then execute additional scripts
     $.ajax({
         url: 'footer.html',
         method: 'GET',
         success: function(response) {
             // اضافه کردن محتوای فوتر به صفحه اصلی
             $('body').append(response);
-        },   
+            
+            // دستوراتی که بعد از لود شدن فوتر باید اجرا شوند
+            const startYear = 2024;
+            const currentYear = new Date().getFullYear();
+            // Set the start year
+            $('#start-year').text(startYear);
+    
+            // Check and set the current year only if it's different from start year
+            if (startYear !== currentYear) {
+                $('#current-year').text('-' + currentYear);
+            } else {
+                // Optionally, you can hide the current year span if they are the same
+                $('#current-year').hide();
+            }
+        },
         error: function(error) {
             console.error('Error loading footer:', error);
         }
     });
 
-    const startYear = 2024;
-    const currentYear = new Date().getFullYear();
-    // Set the start year
-    $('#start-year').text(startYear);
-
-    // Check and set the current year only if it's different from start year
-    if (startYear !== currentYear) {
-        $('#current-year').text('-' + currentYear);
-    } else {
-        // Optionally, you can hide the current year span if they are the same
-        $('#current-year').hide();
-    }
 
     // 3. پیاده‌سازی اسکریپت جاوااسکریپت
     $(document).on('click', '.open', function() {
