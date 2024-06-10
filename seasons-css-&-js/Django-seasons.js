ca87ac5450ea2d60bd551612d83f537c59944fce
MahdiRezaie <103851352+Mahd25@users.noreply.github.com>
2024-06-10 12:59:56 -0700

    adding ajax for year in footer

--- seasons-css-&-js/Django-seasons.js
+++ seasons-css-&-js/Django-seasons.js
@@ -55,6 +55,19 @@ $(document).ready(function() {
             // اضافه کردن محتوای فوتر به صفحه اصلی
             $('body').append(response);
         },
+        const startYear = 2024;
+        const currentYear = new Date().getFullYear();
+
+        // Set the start year
+        $('#start-year').text(startYear);
+
+        // Check and set the current year only if it's different from start year
+        if (startYear !== currentYear) {
+            $('#current-year').text('-' + currentYear);
+        } else {
+            // Optionally, you can hide the current year span if they are the same
+            $('#current-year').hide();
+        },    
         error: function(error) {
             console.error('Error loading footer:', error);
         }
