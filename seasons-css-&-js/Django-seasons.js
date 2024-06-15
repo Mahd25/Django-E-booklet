694691551bbe66f5ad2b0009834da8843738c709
AM-ER team <django.e.booklet01@gmail.com>
2024-06-15 11:13:32 -0700

    Removing JavaScript related to the 'footer' and its components

--- seasons-css-&-js/Django-seasons.js
+++ seasons-css-&-js/Django-seasons.js
@@ -45,35 +45,6 @@ $(document).ready(function () {
             console.error("Error loading menubar:", error);
         }
     });
-// -------------------------------------------------------------------------------------------   
-
-    // Load footer and then execute additional scripts
-    $.ajax({
-        url: 'footer.html',
-        method: 'GET',
-        success: function (response) {
-
-            // Adds the 'footer' to the end of the 'body' tag
-            $('body').append(response);
-
-            const startYear = 2024;
-            const currentYear = new Date().getFullYear();
-
-            // Set the start year
-            $('#start-year').text(startYear);
-
-            // Check and set the current year only if it's different from start year
-            if (startYear !== currentYear) {
-                $('#current-year').text('-' + currentYear);
-            } else {
-                // Optionally, you can hide the current year span if they are the same
-                $('#current-year').hide();
-            }
-        },
-        error: function (error) {
-            console.error('Error loading footer:', error);
-        }
-    });
 // ------------------------------------------------------------------------------------------------------------
 
     // open and close icon for menubar
