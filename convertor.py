de5392fd63d2393e7d303284fbb61bb8838c571d
AM-ER team <django.e.booklet01@gmail.com>
2024-06-17 07:09:37 -0700

    adding 'css and js' path to html structure in convertor.py

--- convertor.py
+++ convertor.py
@@ -99,6 +99,7 @@ def convert_markdown_file(input_file, output_file, title):
 <link rel="icon" href="../Images/favicon.ico" type="image/ico">
 
 <link rel="stylesheet" type="text/css" href="../seasons-css-&-js/seasons-style.css">
+<link rel="stylesheet" type="text/css" href="../seasons-css-&-js/Footer-style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -116,6 +117,7 @@ def convert_markdown_file(input_file, output_file, title):
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="../seasons-css-&-js/Django-seasons.js"></script>
 <script src="../seasons-css-&-js/copy-icon.js"></script>
+<script src="../seasons-css-&-js/Footer-Copyright.js"></script>
 </body>
 </html>
     """
