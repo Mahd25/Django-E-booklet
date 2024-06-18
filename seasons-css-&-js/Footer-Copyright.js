5f0aefc6fd955814e0d53f28ee1816e615dcf233
AM-ER team <django.e.booklet01@gmail.com>
2024-06-18 12:59:29 -0700

    Modification of the Footer-Copyright.js file to fix the problem of the url address and the path of the logo image

--- seasons-css-&-js/Footer-Copyright.js
+++ seasons-css-&-js/Footer-Copyright.js
@@ -1,15 +1,40 @@
 $(document).ready(function() {
+    // تابع برای تعیین مسیر صحیح فایل footer.html
+    function getFooterUrl() {
+        var currentPath = window.location.pathname;
+        if (currentPath.includes('/seasons-html/')) {
+            return 'footer.html'; // مسیر برای فایل‌های موجود در پوشه seasons-html
+        } else {
+            return 'seasons-html/footer.html'; // مسیر برای فایل‌های موجود در سطح بالاتر
+        }
+    }
+
     // Load footer and then execute additional scripts
     $.ajax({
-        url: 'footer.html',
+        url: getFooterUrl(),
         method: 'GET',
         success: function (response) {
-
             // Adds the 'footer' to the end of the 'body' tag
             $('body').append(response);
-        
-        // ---------------------------------------------------------------------------
+            
+            // تابع برای تعیین مسیر صحیح تصویر
+            function setFooterImagePath() {
+                var footerImg = document.getElementById('footer-img');
+                
+                // مکان نسبی فایل HTML فعلی را تعیین کنید
+                var currentPath = window.location.pathname;
 
+                // بر اساس مکان نسبی، مسیر تصویر را تنظیم کنید
+                if (currentPath.includes('/seasons-html/')) {
+                    footerImg.src = '../Images/my-logo.svg'; // تنظیم مسیر برای فایل‌های موجود در پوشه seasons-html
+                } else {
+                    footerImg.src = 'Images/my-logo.svg'; // تنظیم مسیر برای فایل e-booklet.html
+                }
+            }
+
+            // فراخوانی تابع پس از بارگذاری فوتر
+            setFooterImagePath();
+        
             const startYear = 2024;
             const currentYear = new Date().getFullYear();
 
@@ -28,4 +53,4 @@ $(document).ready(function() {
             console.error('Error loading footer:', error);
         }
     });
-})
+});
